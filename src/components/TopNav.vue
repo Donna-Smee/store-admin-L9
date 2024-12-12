b34bae39245282b39619a75e01dfacac777d1b35
Donna-Smee <dailydosesofdonuts@gmail.com>
2024-12-12 02:47:35 -0500

    Add product pictures

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/bestbuybackground.png" alt="Algonquin Pet Store Logo">
       </a>
       Admin Portal :D :D
     </div>
